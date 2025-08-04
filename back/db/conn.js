797bc57e73b03c4b551dc92d16ebcdbc190005ff
BRENDA JULYA DE SOUZA FREITAS DA SILVA <brenda_j_silva@estudante.sesisenai.org.br>
2025-08-04 11:34:14 -0300

    nona versão

--- back/db/conn.js
+++ back/db/conn.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port:3306,
+    port:process.env.DB_PORT,
   }
 );
 
